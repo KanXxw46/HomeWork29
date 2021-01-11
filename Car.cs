c5db3cf149bb8f6bcfe72be7ea8a0b0d5b9fc154
Андрей Кан <1kan_andreikkkk@mail.ru>
2021-01-11 09:45:13 +0600

    29

---
+++ Car.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw29_Paterns
+{
+    
+    class Trakrot : CarDoing
+    {
+        public override void AddBox()
+        {
+            Do.box = new Box() { Name = "Автомат" };
+        }
+
+        public override void AddWheels()
+        {
+            Do.wheels = new Wheels() { Name = "Для тракторов(большие)" };
+        }
+
+        public override void AddMotor()
+        {
+            Do.motor = new Motor() { Name = "360 л.c" };
+        }
+
+        public override void AddBody()
+        {
+            Do.body = new Body() { Name = "Алюминивое" };
+        }
+    }
+    class Mechanik
+    {
+        public CarDoing Make(CarBuilder carBuilder)
+        {
+            carBuilder.AddBox();
+            carBuilder.AddWheels();
+            carBuilder.AddMotor();
+            carBuilder.AddBody();
+            return carBuilder.Do;
+        }
+    }
+    abstract class CarBuilder
+    {
+        public CarDoing Do { get; set; }
+        public void CreateDrink()
+        {
+            Do = new CarDoing();
+        }
+        public abstract void AddBox();
+        public abstract void AddWheels();
+        public abstract void AddMotor();
+        public abstract void AddBody();
+
+    }
+    class CarDoing
+    {
+        public Box box { get; set; }
+        public Wheels wheels { get; set; }
+        public Motor motor { get; set; }
+        public Body body { get; set; }
+        public override string ToString()
+        {
+            string result = box.Name + Environment.NewLine +
+                wheels.Name + Environment.NewLine +
+                motor.Name + Environment.NewLine +
+                body.Name + Environment.NewLine;
+            return result;
+        }
+    }
+    class Box
+    {
+        public string Name { get; set; }
+    }
+    class Wheels
+    {
+        public string Name { get; set; }
+    }
+
+    class Motor
+    {
+        public string Name { get; set; }
+    }
+
+    class Body
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hw29_Paterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mechanik mechanik = new Mechanik();
+            CarBuilder carBuilder =  new Trakrot();
+            var traktor = mechanik.Make(carBuilder);
+            Console.WriteLine(traktor.ToString());
+        }
+    }
+}
